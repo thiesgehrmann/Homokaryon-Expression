b4e5eeb75b05c5f6fcb275d82161b506758683ef
Thies Gehrmann <t.gehrmann@tudelft.nl>
2016-11-25 10:34:25 +0100

    Finished example

--- example_data/gen_example_samples.sh
+++ example_data/gen_example_samples.sh
@@ -23,10 +23,10 @@ function makesample() {
   name=$5
 
   for i in `seq 1 $na`; do
-    makeread $seqa "$name:a" $i
+    makeread $seqa "${name}:a" $i
   done
   for i in `seq 1 $nb`; do
-    makeread $seqb "$name:b" $i
+    makeread $seqb "${name}:b" $i
   done
 
 }
@@ -34,11 +34,11 @@ function makesample() {
 function makeread() {
 
   seq=$1
-  name=$2
+  local name=$2
   num=$3
   len=${#seq}
 
-  echo "@$name:$num"
+  echo "@${name}:${num}"
   echo $seq
   echo "+"
   head -c $len < /dev/zero | tr '\0' 'I'
@@ -59,17 +59,17 @@ seqa2=`getseqn $g1_file 2`
 seqb1=`getseqn $g2_file 1`
 seqb2=`getseqn $g2_file 2`
 
-makesample $seqa1 $seqb1 10 44 sample1_1 > sample1_1.fastq
-makesample $seqa2 $seqb2 15 5 sample1_1 >> sample1_1.fastq
+makesample $seqa1 $seqb1 100 440 sample1_1 > sample1_1.fastq
+makesample $seqa2 $seqb2 150 50 sample1_1 >> sample1_1.fastq
 
-makesample $seqa1 $seqb1 12 40 sample1_2 > sample1_2.fastq
-makesample $seqa2 $seqb2 14 6 sample1_2 >> sample1_2.fastq
+makesample $seqa1 $seqb1 120 400 sample1_2 > sample1_2.fastq
+makesample $seqa2 $seqb2 140 60 sample1_2 >> sample1_2.fastq
 
-makesample $seqa1 $seqb1 40 10 sample2_1 > sample2_1.fastq
-makesample $seqa2 $seqb2 4 12 sample2_1 >> sample2_1.fastq
+makesample $seqa1 $seqb1 400 100 sample2_1 > sample2_1.fastq
+makesample $seqa2 $seqb2 40 120 sample2_1 >> sample2_1.fastq
 
-makesample $seqa1 $seqb1 44 12 sample2_2 > sample2_2.fastq
-makesample $seqa2 $seqb2 6 10 sample2_2 >> sample2_2.fastq
+makesample $seqa1 $seqb1 440 120 sample2_2 > sample2_2.fastq
+makesample $seqa2 $seqb2 60 100 sample2_2 >> sample2_2.fastq
 
 java -Xmx10G -jar $PICARD  FastqToSam "FASTQ=./sample1_1.fastq" "OUTPUT=./sample1_1.bam" "SAMPLE_NAME=sample1_1"
 java -Xmx10G -jar $PICARD  FastqToSam "FASTQ=./sample1_2.fastq" "OUTPUT=./sample1_2.bam" "SAMPLE_NAME=sample1_2"
